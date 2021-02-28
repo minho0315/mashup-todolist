9a34dbab7ab2f9ef5ebbaebbf71f63ea2ebc1923
minho0315 <dlalsgh369@naver.com>
2021-02-28 20:26:44 +0900

    Function implementation

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import styled, {css} from 'styled-components';
 import {MdAdd} from 'react-icons/md';
+import {useTodoDispatch, useTodoNextId} from '../TodoContext';
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -27,9 +28,6 @@ const CircleButton = styled.button`
   border-radius: 50%;
   border: none;
   outline: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
   transition: 0.125s all ease-in;
   ${props =>
@@ -72,22 +70,44 @@ const Input = styled.input`
   width: 100%;
   outline: none;
   font-size: 18px;
-  box-sizing: border-border-box;
+  box-sizing: border-box;
 `;
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
 
     const onToggle = () => setOpen(!open);
+    const onChange = e => setValue(e.target.value);
+    const onSubmit = e => {
+        e.preventDefault(); //새로고침 방지
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false
+        }
+    });
+		setValue('');
+		setOpen(false);
+		nextId.current += 1;
+	};
 
     return (
       <>
         {open && (
           <InsertFormPositioner>
-            <InsertForm>
+            <InsertForm onSubmit={onSubmit}>
               <Input
                 autoFocus
-                placeholder="할 일을 입력 후, Enter 를 누르세요"/>
+                placeholder="할 일을 입력 후, Enter 를 누르세요"
+								onChange={onChange}
+								value={value}
+							/>
             </InsertForm>
           </InsertFormPositioner>
         )}
@@ -98,4 +118,4 @@ function TodoCreate() {
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {MdDone, MdDelete} from 'react-icons/md';
+import { useTodoDispatch} from '../TodoContext';
 
 const Remove = styled.div`
   display: flex;
@@ -12,7 +13,6 @@ const Remove = styled.div`
   &:hover {
       color: #ff6b6b;
   }
-  display: none;
 `;
 
 const TodoItemBlock = styled.div`
@@ -22,7 +22,7 @@ const TodoItemBlock = styled.div`
   padding-bottom: 12px;
   &:hover {
       ${Remove} {
-          display: initial;
+          opacity: 1;
       }
   }
 `;
@@ -58,15 +58,21 @@ const Text = styled.div`
 `;
 
 function TodoItem({id, done, text}) {
-    return(
-        <TodoItemBlock>
-          <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
-          <Text done={done}>{text}</Text>
-          <Remove>
-            <MdDelete />
-          </Remove>
-        </TodoItemBlock>
-    );
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({type: 'TOGGLE', id});
+  const onRemove = () => dispatch({type: 'REMOVE', id});
+  
+  return (
+    <TodoItemBlock>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove onClick={onRemove}>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
